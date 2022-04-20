d95dcbcdebf6a2d116785b68a9caee6822672e13
Abhi <abhishekchauhan9036@gmail.com>
2022-04-20 06:25:58 +0530

    commit message

--- AAA_DSA_Mock_Interview_Questions/Solution/Stack/Next-Greater-Element.java
+++ AAA_DSA_Mock_Interview_Questions/Solution/Stack/Next-Greater-Element.java
@@ -1,9 +1,9 @@
 import java.util.*;
-class Main{
+public class Main{
 	public static void nextGreaterElement(int arr[])
 	{
 		Stack<Integer> s = new Stack<>();
-		int nge[] = new int[arr.length];
+		int arr2[] = new int[arr.length];
 		for (int i = arr.length - 1; i >= 0; i--)
 		{
 			if (!s.empty())
@@ -14,16 +14,16 @@ class Main{
 					s.pop();
 				}
 			}
-			nge[i] = s.empty() ? -1 : s.peek();
+			arr2[i] = s.empty() ? -1 : s.peek();
 			s.push(arr[i]);
 		}
 		for (int i = 0; i < arr.length; i++)
-			System.out.println(arr[i] +" --> " + nge[i]);
+			System.out.println(arr[i] + " -------> " + arr2[i]);
 	}
 
 	public static void main(String[] args)
 	{
-		int arr[] = { 11, 13, 21, 3 };
+        int arr[] = { 11, 13, 21, 3 };
 		nextGreaterElement(arr);
 	}
 }
